b781ba5e5a59d4f7a13556c09fd54ef81d97aba0
Jonus <joclapshaw@gmail.com>
2025-10-28 12:01:19 -0700

    m

--- app.js
+++ app.js
@@ -6,18 +6,47 @@ const app = express();
 // Enable static file serving
 app.use(express.static('public'));
 
+const orders = [];
 //Define the port number where our server will listen 
 const PORT = 3001;
+
+app.set ('view engine', 'ejs');
+
+app.use (express.urlencoded({ extended: true }));
 
 //Define a default "route" ('/')
 //req: contains information about the incoming request
 //res: allows us to send back a response to the client
 app.get('/', (req, res) => {
     //res.send('Welcome to ice cream!');
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
-})
+    res.render('home');
+});
+
+app.get('/confirm', (req, res) => {
+    res.render('confirmation');
+});
+
+app.get('/admin', (req, res) => {
+    res.render('admin');
+});
+
+app.post('/submit-order', (req, res) => {
+    const order = {
+        fname: req.body.fname,
+        email: req.body.email,
+        flavor: req.body.flavor,
+        method: req.body.method,
+        toppings: req.body.toppings,
+    }
+
+    orders.push(order);
+    console.log(orders);
+
+    res.render('confirmation', {order});
+
+});
 
 //Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running at http:localhost:${PORT}`);
-})
+});
