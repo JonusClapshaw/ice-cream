9bf565036992ec4c1c7d8e7ccbac0fa65cb902eb
samkosal <67755908+samkosal@users.noreply.github.com>
2025-11-18 12:03:02 -0800

    ignore

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.post('/submit-order', (req, res) => {
 
 //Start the server and listen on the specified port
 app.listen(PORT, () => {
-    console.log(`Server is running at http:localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
